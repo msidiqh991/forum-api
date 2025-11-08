00ed2942f0aac8900a5f01e0039301af50d7fdf3
Muhammad Sidiq Hardiansyah <msidiqh991@gmail.com>
2025-11-08 17:56:39 +0700

    feat: implement CommentRepository class and its test suite

---
+++ src/Domains/comments/CommentRepository.js
@@ -0,0 +1,15 @@
+class CommentRepository {
+    async addComment(comment) {
+        throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+    }
+
+    async verifyCommentAvailability(commentId) {
+        throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+    }
+
+    async deleteComment(commentId) {
+        throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+    }
+}
+
+module.exports = CommentRepository;
