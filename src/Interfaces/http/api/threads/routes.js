a742e0cbff65194e922a96e6fe408c69ebab05f9
Muhammad Sidiq Hardiansyah <msidiqh991@gmail.com>
2025-11-08 17:51:39 +0700

    feat: add routes for comments and replies with authentication

--- src/Interfaces/http/api/threads/routes.js
+++ src/Interfaces/http/api/threads/routes.js
@@ -1,24 +1,20 @@
-const routes = (handler) => ([
-    {
-        method: 'POST',
-        path: '/threads',
-        handler: handler.postThreadHandler,
+const routes = (handler) => [
+  {
+    method: "POST",
+    path: "/threads",
+    handler: handler.postThreadHandler,
+    options: {
+      auth: "forumapi_jwt",
     },
-    {
-        method: 'POST',
-        path: '/threads/{threadId}/comments',
-        handler: handler.postCommentHandler,
+  },
+  {
+    method: "GET",
+    path: "/threads/{threadId}",
+    handler: handler.getThreadDetailHandler,
+    options: {
+      auth: "forumapi_jwt",
     },
-    {
-        method: 'DELETE',
-        path: '/threads/{threadId}/comments/{commentId}',
-        handler: handler.deleteCommentHandler,
-    },
-    {
-        method: 'GET',
-        path: '/threads/{threadId}',
-        handler: handler.getThreadDetailHandler,
-    }
-])
+  },
+];
 
-module.exports = routes
+module.exports = routes;
