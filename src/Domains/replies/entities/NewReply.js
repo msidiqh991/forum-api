fc13d5d0ef52a60dca3c4f068d39fc1da085404e
Muhammad Sidiq Hardiansyah <msidiqh991@gmail.com>
2025-11-11 20:56:09 +0700

    feat: enhance NewReply entity to validate owner and commentId properties, and update tests accordingly

--- src/Domains/replies/entities/NewReply.js
+++ src/Domains/replies/entities/NewReply.js
@@ -1,19 +1,29 @@
 class NewReply {
-    constructor(payload) {
-        this._verifyPayload(payload);
-        const { content } = payload;
-        this.content = content;
+  constructor(payload) {
+    this._verifyPayload(payload);
+    const { content, owner, commentId } = payload;
+    this.content = content;
+    this.owner = owner;
+    this.commentId = commentId;
+  }
+
+  _verifyPayload({ content, owner, commentId }) {
+    if (!content || content.length === 0 || !owner || !commentId) {
+      throw new Error("NEW_REPLY.NOT_CONTAIN_VALID_CONTENT");
     }
 
-    _verifyPayload({ content }) {
-        if (!content || content.length === 0) {
-            throw new Error('NEW_REPLY.NOT_CONTAIN_VALID_CONTENT');
-        }
-
-        if (typeof content !== 'string') {
-            throw new Error('NEW_REPLY.NOT_CONTAIN_VALID_CONTENT');
-        }
+    if (
+      typeof content !== "string" ||
+      typeof owner !== "string" ||
+      typeof commentId !== "string"
+    ) {
+      throw new Error("NEW_REPLY.NOT_CONTAIN_VALID_CONTENT");
     }
+
+    if (content.trim().length === 0) {
+      throw new Error("NEW_REPLY.NOT_CONTAIN_VALID_CONTENT");
+    }
+  }
 }
 
 module.exports = NewReply;
--- src/Domains/replies/entities/_tests/NewReply.test.js
+++ src/Domains/replies/entities/_tests/NewReply.test.js
@@ -8,16 +8,38 @@ describe('NewReply entities', () => {
     })
 
     it('should throw error when payload not meet data type specification', () => {
-        const payload = { content: 12345 };
+        const payload = { 
+            content: 12345,
+            owner: {},
+            commentId: [],
+        };
+        
         expect(() => new NewReply(payload))
             .toThrowError('NEW_REPLY.NOT_CONTAIN_VALID_CONTENT');
     })
 
     it('should create NewReply object correctly', () => {
-        const payload = { content: 'Example content of a new reply' };
+        const payload = { 
+            content: 'Example content of a new reply',
+            owner: 'user-123',
+            commentId: 'comment-123',
+        };
         const newReply = new NewReply(payload);
 
         expect(newReply).toBeInstanceOf(NewReply);
         expect(newReply.content).toEqual(payload.content);
+        expect(newReply.owner).toEqual(payload.owner);
+        expect(newReply.commentId).toEqual(payload.commentId);
+    })
+
+    it('should throw error when content is only spaces', () => {
+        const payload = { 
+            content: '     ',
+            owner: 'user-123',
+            commentId: 'comment-123',
+        };
+        
+        expect(() => new NewReply(payload))
+            .toThrowError('NEW_REPLY.NOT_CONTAIN_VALID_CONTENT');
     })
 })
