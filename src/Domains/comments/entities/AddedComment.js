263254c3d9e3754bfc9999c9eb0f2e8c1b2c6b98
Muhammad Sidiq Hardiansyah <msidiqh991@gmail.com>
2025-11-11 20:55:57 +0700

    feat: add owner property validation to AddedComment and implement corresponding tests

--- src/Domains/comments/entities/AddedComment.js
+++ src/Domains/comments/entities/AddedComment.js
@@ -1,21 +1,26 @@
 class AddedComment {
-    constructor(payload) {
-        this._verifyPayload(payload);
-        const { id, content } = payload;
+  constructor(payload) {
+    this._verifyPayload(payload);
+    const { id, content, owner } = payload;
 
-        this.id = id;
-        this.content = content;
+    this.id = id;
+    this.content = content;
+    this.owner = owner;
+  }
+
+  _verifyPayload({ id, content, owner }) {
+    if (!id || !content || !owner) {
+      throw new Error("ADDED_COMMENT.NOT_CONTAIN_NEEDED_PROPERTY");
     }
 
-    _verifyPayload({ id, content }) {
-        if (!id || !content) {
-            throw new Error('ADDED_COMMENT.NOT_CONTAIN_NEEDED_PROPERTY');
-        }
-
-        if (typeof id !== 'string' || typeof content !== 'string') {
-            throw new Error('ADDED_COMMENT.NOT_MEET_DATA_TYPE_SPECIFICATION');
-        }
+    if (
+      typeof id !== "string" ||
+      typeof owner !== "string" ||
+      typeof content !== "string"
+    ) {
+      throw new Error("ADDED_COMMENT.NOT_MEET_DATA_TYPE_SPECIFICATION");
     }
+  }
 }
 
 module.exports = AddedComment;
---
+++ src/Domains/comments/entities/_test/AddedComment.test.js
@@ -0,0 +1,38 @@
+const AddedComment = require("../AddedComment");
+
+describe("a AddedComment entities", () => {
+  it("should throw error when payload did not contain needed property", () => {
+    const payload = {
+      id: "comment-123",
+    };
+
+    expect(() => new AddedComment(payload)).toThrowError(
+      "ADDED_COMMENT.NOT_CONTAIN_NEEDED_PROPERTY"
+    );
+  });
+
+  it("should throw error when payload not meet data type specification", () => {
+    const payload = {
+      id: "comment-123",
+      content: 123,
+      owner: 456,
+    };
+
+    expect(() => new AddedComment(payload)).toThrowError(
+      "ADDED_COMMENT.NOT_MEET_DATA_TYPE_SPECIFICATION"
+    );
+  });
+
+  it("should create AddedComment object correctly", () => {
+    const payload = {
+      id: "comment-123",
+      content: "This is a comment",
+      owner: "user-123",
+    };
+
+    const addedComment = new AddedComment(payload);
+    expect(addedComment.id).toEqual(payload.id);
+    expect(addedComment.content).toEqual(payload.content);
+    expect(addedComment.owner).toEqual(payload.owner);
+  });
+});
