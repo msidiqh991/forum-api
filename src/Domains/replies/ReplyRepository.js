1e0e6b17306369d6b907b7611cdd120e5083e75d
Muhammad Sidiq Hardiansyah <msidiqh991@gmail.com>
2025-11-11 20:55:45 +0700

    feat: implement ReplyRepository and AddedReply entities with corresponding tests

---
+++ src/Domains/replies/ReplyRepository.js
@@ -0,0 +1,19 @@
+class ReplyRepository {
+    async addReply(reply) {
+        throw new Error('REPLY_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+    }
+
+    async verifyReplyAvailability(replyId) {
+        throw new Error('REPLY_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+    }
+
+    async verifyReplyOwner(replyId, owner) {
+        throw new Error('REPLY_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+    }
+
+    async deleteReply(replyId) {
+        throw new Error('REPLY_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+    }
+}
+
+module.exports = ReplyRepository;
---
+++ src/Domains/replies/entities/_tests/AddedReply.test.js
@@ -0,0 +1,35 @@
+const AddedReply = require('../AddedReply');
+
+describe('AddedReply entities', () => {
+    it('should throw error when payload did not contain needed property', () => {
+        const payload = {
+            id: 'reply-123',
+        };
+
+        expect(() => new AddedReply(payload)).toThrowError('ADDED_REPLY.NOT_CONTAIN_NEEDED_PROPERTY');
+    })
+
+    it('should throw error when payload not meet data type specification', () => {
+        const payload = {
+            id: 'reply-123',
+            content: 123,
+            owner: 456,
+        };
+        expect(() => new AddedReply(payload)).toThrowError('ADDED_REPLY.NOT_MEET_DATA_TYPE_SPECIFICATION');
+    });
+
+    it('should create AddedReply object correctly', () => {
+        const payload = {
+            id: 'reply-123',
+            content: 'This is a reply',
+            owner: 'user-123',
+        };
+
+        const addedReply = new AddedReply(payload);
+        
+        expect(addedReply).toBeInstanceOf(AddedReply);
+        expect(addedReply.id).toEqual(payload.id);
+        expect(addedReply.content).toEqual(payload.content);
+        expect(addedReply.owner).toEqual(payload.owner);
+    });
+});
