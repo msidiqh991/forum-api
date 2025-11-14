913f704b94810fc50e220abca1df2b2094c9d81a
Muhammad Sidiq Hardiansyah <msidiqh991@gmail.com>
2025-11-14 20:05:17 +0700

    feat: refactor getThreadById method to return thread details without comments and replies

--- src/Infrastructures/repository/ThreadRepositoryPostgres.js
+++ src/Infrastructures/repository/ThreadRepositoryPostgres.js
@@ -46,64 +46,12 @@ class ThreadRepositoryPostgres extends ThreadRepository {
       values: [threadId],
     }
 
-    const commentQuery = {
-      text: `SELECT 
-              c.id, c.content, c.date, u.username,
-            CASE WHEN c.is_deleted = true THEN true ELSE false END AS is_deleted
-            FROM comments c
-            INNER JOIN users u ON c.owner = u.id
-            WHERE c.thread_id = $1
-            ORDER BY c.date ASC`,
-      values: [threadId],
-    }
-
-    const replyQuery = {
-      text: `SELECT
-              r.id, r.content, r.date, r.comment_id, r.is_deleted, u.username
-            FROM replies r
-            INNER JOIN users u on r.owner = u.id
-            WHERE r.comment_id IN (SELECT id FROM comments WHERE thread_id = $1)
-            ORDER BY r.date ASC`,
-      values: [threadId],
-    }
-
-    const threadResult = await this._pool.query(threadQuery);
-
-    if (!threadResult.rowCount) {
+    const result = await this._pool.query(threadQuery);
+    if (!result.rowCount) {
       throw new NotFoundError("thread tidak ditemukan");
     }
-    
-    const thread = threadResult.rows[0];
-    const commentResult = await this._pool.query(commentQuery);
-    const repliesResult = await this._pool.query(replyQuery);
 
-    const comments = commentResult.rows.map((comment) => {
-      const replies = repliesResult.rows
-        .filter(reply => reply.comment_id === comment.id)
-        .map(reply => ({
-          id: reply.id,
-          content: reply.is_deleted ? '**balasan telah dihapus**' : reply.content,
-          date: reply.date,
-          username: reply.username,
-        }))
-
-      return {
-        id: comment.id,
-        username: comment.username,
-        date: comment.date,
-        content: comment.is_deleted ? '**komentar telah dihapus**' : comment.content,
-        replies,
-      }
-    });
-
-    return {
-      id: thread.id,
-      title: thread.title,
-      body: thread.body,
-      date: thread.date,
-      username: thread.username,
-      comments,
-    };
+    return result.rows[0];
   }
 }
 
--- src/Infrastructures/repository/_test/ThreadRepositoryPostgres.test.js
+++ src/Infrastructures/repository/_test/ThreadRepositoryPostgres.test.js
@@ -82,39 +82,6 @@ describe("ThreadRepositoryPostgres", () => {
   });
 
   describe("getThreadById function", () => {
-    it("should return thread with comments and replies when thread is found", async () => {
-      await ThreadTableTestHelper.addThread({
-        id: "thread-123",
-        title: "sebuah thread",
-        body: "sebuah body thread",
-        owner: "user-123",
-      });
-
-      await CommentTableTestHelper.addComment({
-        id: "comment-123",
-        threadId: "thread-123",
-        content: "sebuah comment",
-        owner: "user-123",
-      });
-
-      await ReplyTableTestHelper.addReply({
-        id: "reply-123",
-        commentId: "comment-123",
-        content: "sebuah balasan",
-        owner: "user-123",
-      });
-
-      const threadRepositoryPostgres = new ThreadRepositoryPostgres(
-        pool,
-        () => {}
-      );
-      const thread = await threadRepositoryPostgres.getThreadById("thread-123");
-
-      expect(thread.comments).toHaveLength(1);
-      expect(thread.comments[0].replies).toHaveLength(1);
-      expect(thread.comments[0].replies[0].content).toEqual("sebuah balasan");
-    });
-
     it("should throw NotFoundError when thread not found", async () => {
       const threadRepositoryPostgres = new ThreadRepositoryPostgres(
         pool,
@@ -128,40 +95,29 @@ describe("ThreadRepositoryPostgres", () => {
       ).rejects.toThrowError(NotFoundError);
     });
 
-    it("should return deleted comment and reply content replace with placeholder", async () => {
+    it("should return thread correctly when thread is found", async () => {
+      // Arrange
       await ThreadTableTestHelper.addThread({
-        id: "thread-999",
-        title: "thread terhapus",
-        body: "body thread terhapus",
+        id: "thread-123",
+        title: "sebuah thread",
+        body: "sebuah body thread",
+        date: "2024-01-01T00:00:00.000Z",
         owner: "user-123",
       });
 
-      await CommentTableTestHelper.addComment({
-        id: "comment-999",
-        threadId: "thread-999",
-        content: "**komentar telah dihapus**",
-        owner: "user-123",
-        is_deleted: true,
+      const threadRepositoryPostgres = new ThreadRepositoryPostgres(pool, {});
+
+      // Action
+      const thread = await threadRepositoryPostgres.getThreadById("thread-123");
+
+      // Assert
+      expect(thread).toStrictEqual({
+        id: "thread-123",
+        title: "sebuah thread",
+        body: "sebuah body thread",
+        date: "2024-01-01T00:00:00.000Z",
+        username: "dicoding",
       });
-
-      await ReplyTableTestHelper.addReply({
-        id: "reply-999",
-        commentId: "comment-999",
-        content: "**balasan telah dihapus**",
-        owner: "user-123",
-        is_deleted: true,
-      });
-
-      const threadRepositoryPostgres = new ThreadRepositoryPostgres(
-        pool,
-        () => {}
-      );
-      const thread = await threadRepositoryPostgres.getThreadById("thread-999");
-
-      expect(thread.comments).toHaveLength(1);
-      expect(thread.comments[0].content).toEqual("**komentar telah dihapus**");
-      expect(thread.comments[0].replies).toHaveLength(1);
-      expect(thread.comments[0].replies[0].content).toEqual("**balasan telah dihapus**");
     });
   });
 });
