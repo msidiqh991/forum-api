0d8d9602b30dbf4949f2557d3fad1735e78ec469
Muhammad Sidiq Hardiansyah <msidiqh991@gmail.com>
2025-11-19 22:29:40 +0700

    feat: add say hello world test case

--- src/Infrastructures/http/_test/createServer.test.js
+++ src/Infrastructures/http/_test/createServer.test.js
@@ -17,6 +17,23 @@ describe("HTTP server", () => {
     expect(response.statusCode).toEqual(404);
   });
 
+  describe("when GET /", () => {
+    it('should response 200 and "Hello World!"', async () => {
+      // Arrange
+      const server = await createServer({});
+
+      const response = await server.inject({
+        method: "GET",
+        url: "/",
+      })
+
+      const responseJson = JSON.parse(response.payload);
+      
+      expect(response.statusCode).toEqual(200);
+      expect(responseJson.value).toEqual("Hello World!");
+    })
+  })
+
   it("should handle server error correctly", async () => {
     // Arrange
     const requestPayload = {
